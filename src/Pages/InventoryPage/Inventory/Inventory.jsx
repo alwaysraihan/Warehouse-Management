1639da69f86bb00a4a54cace44a33a917e5ac1d2
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-05-07 20:45:29 +0600

    slider and admin pannel more funtionality added

--- src/Pages/InventoryPage/Inventory/Inventory.jsx
+++ src/Pages/InventoryPage/Inventory/Inventory.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { NavLink, useParams } from "react-router-dom";
+import { Link, NavLink, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import auth from "../../../Firebase/firebase.init";
 import Slider from "../slider/Slider";
+import { signOut } from "firebase/auth";
 
 const Inventory = () => {
   const [user] = useAuthState(auth);
   const { id } = useParams();
   const [inventoryItem, setEnventoryItem] = useState([]);
+  const handleSignOut = () => {
+    signOut(auth);
+  };
   const [stockNumber, setStockNumber] = useState({
     stock: "",
   });
@@ -103,19 +107,57 @@ const Inventory = () => {
                   <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-green-500 rounded-full"></span>
                   <span className="absolute top-0 right-0 h-2 w-2 mt-1 mr-2 bg-green-500 rounded-full animate-ping"></span>
                 </div>
-                {user?.photoURL ? (
-                  <img
-                    className="w-16 h-16 rounded-full border-2 border-blue-600"
-                    src={user.photoURL}
-                    alt="profile"
-                  />
-                ) : (
-                  <img
-                    className="w-16 h-16 rounded-full border-2 border-blue-600"
-                    src="https://i.ibb.co/KDfw63R/Pngtree-business-male-icon-vector-4187852.png"
-                    alt="profile"
-                  />
-                )}
+                <div className="antialiased">
+                  <div className="relative cursor-pointer group">
+                    {user?.photoURL ? (
+                      <img
+                        className="w-16 h-16 rounded-full border-2 border-blue-600"
+                        src={user.photoURL}
+                        alt="profile"
+                      />
+                    ) : (
+                      <img
+                        className="w-16 h-16 rounded-full border-2 border-blue-600"
+                        src="https://i.ibb.co/KDfw63R/Pngtree-business-male-icon-vector-4187852.png"
+                        alt="profile"
+                      />
+                    )}
+
+                    <div className="absolute   hidden right-0 top-0 mt-10 bg-white rounded-md shadow-lg  group-hover:block hover:block">
+                      <div>
+                        <div class="flex flex-col p-4 space-y-1 font-medium border-b">
+                          <span class="text-gray-800">
+                            {user.displayName ? user.displayName : "User"}
+                          </span>
+                          <span class="text-sm text-gray-400">
+                            {user.email ? user.email : "dealer@example.com"}
+                          </span>
+                        </div>
+                        <ul class="flex flex-col p-2 my-2 space-y-1">
+                          <li>
+                            <Link
+                              to="/"
+                              class="block px-2 py-1 text-teal-600  transition rounded-md hover:bg-gray-100"
+                            >
+                              Home Page
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              to="/dashboard/manage-inventory"
+                              class="block px-2 py-1  text-teal-600 transition rounded-md hover:bg-gray-100"
+                            >
+                              Manage Inventory
+                            </Link>
+                          </li>
+                        </ul>
+                        <div class="flex items-center text-xl justify-center p-4 text-blue-600 underline border-t">
+                          <span onClick={handleSignOut}>Logout</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
